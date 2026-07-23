a8348fd5bd0f611f2ed66fae0ec12257d4eb4323
miroElbehery <miroelbehery178@gmail.com>
2026-07-23 11:32:20 +0300

    feat: get user data for greetings and update clock every min

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -2,12 +2,20 @@ import { DashboardGreeting } from "@/components/dashboard/dashboard-greeting";
 import { DashboardStats } from "@/components/dashboard/dashboard-stats";
 import { ProjectTaskList } from "@/components/dashboard/project-task-list";
 import { PrivateTaskList } from "@/components/dashboard/private-task-list";
+import { createClient } from "@/utils/supabase/server";
+import { cookies } from "next/headers";
 
-export default function Page() {
+export default async function Page() {
+     const cookieStore = await cookies();
+     const supabase = createClient(cookieStore);
+     const {
+          data: { user },
+     } = await supabase.auth.getUser();
+     
   return (
     <div className="flex bg-primary-foreground min-h-screen flex-col p-8">
       <div className="mb-8">
-        <DashboardGreeting />
+        <DashboardGreeting userName={user?.user_metadata?.full_name.split(" ")[0]} />
       </div>
 
       <div className="grid grid-cols-1 gap-8 lg:grid-cols-12">
